60ca973af2161cd5b805f15c49996dd0baaa25f3
Kandappan <kandappanva@gmail.com>
2023-12-27 08:56:07 +0530

    minor

--- 01-basics/matmul.cpp
+++ 01-basics/matmul.cpp
@@ -24,6 +24,14 @@ int main(){
     std::cout << "Time taken by method 1: "
          << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j < 100; j++)
+        {
+            A[i][j] = i + j;
+            B[i][j] = i - j;
+            C[i][j] = 0;
+        }
+
     // Method 2:
     start = high_resolution_clock::now();
     for (int j = 0; j < 100; j++)
