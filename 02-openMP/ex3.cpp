be57125ea3c0c4c54f27b74bc9e27d1f86fc7391
Kandappan <kandappanva@gmail.com>
2023-12-27 23:48:33 +0530

    updated ex3.cpp with serial

--- 02-openMP/ex3.cpp
+++ 02-openMP/ex3.cpp
@@ -11,12 +11,31 @@ using namespace std;
 int main(int argc, char **argv)
 {
     float *input_array;
-    size_t N = atoi(argv[1]); 
+    bool flag=false;
+    size_t N = pow(2,atoi(argv[1]));
+    std::cout << "System size N = " << N << std::endl;
+    float search_num = atof(argv[2]);
     // creating memory for array of size N
     input_array = (float *)malloc(sizeof(float) * N);
+
+    for (int i = 0; i < N; i++)
+        input_array[i] = i+1.0;
+
+    // Serial implementation
+    for (int i = 0; i < N; i++)
+        if (input_array[i] == search_num)
+            flag = true;
+    if (flag)
+        std::cout << "Element found ." << std::endl;
+    else 
+        std::cout << "Element not found ." <<std::endl; 
     // Your code goes here
-    // Implement a parallel version of Linear search
+    // Implement a parallel version of Linear search (naive)
+
+    
+    // Implement a parallel version of Linear search (chunked)
 
 
+    // Your code goes till here
     return 0;
 }
